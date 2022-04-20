ab6fc9e5bb0abe41f47fe488928ea1a85ccc6c8a
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-20 17:32:16 +0200

    Aggiunte tutte le verifiche del caso a Pari&Dispari (Fine)

--- paridispari/script.js
+++ paridispari/script.js
@@ -7,30 +7,57 @@
     Bonus: L’inserimento avviene tramite un campo input
 */
 
-const sceltaUtente = prompt("Scegli pari o dispari");
-const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// Dichiaro le due variabili che devo chiedere all'utente.
+let sceltaUtente;
+let numeroUtente;
 
+// Dichiaro la Booleana che mi serve per il primo controllo.
+let scrittoBene = false;
+
+// Controllo se pari e dispari sono scritti bene.
+while(!scrittoBene){
+    sceltaUtente = prompt("Scegli pari o dispari").trim();
+    if(sceltaUtente === "pari" || sceltaUtente === "dispari"){
+        scrittoBene = true;
+    }
+}
+
+// Controllo che l'utente abbia inserito un numero scritto in cifre da 1 a 5.
+while(isNaN(numeroUtente)){
+   numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+}
+while(numeroUtente<1 || numeroUtente>5){
+   numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+}
+
+// Dichiaro la funzione per ottenere un numero casuale.
 function getRandom (max){
     return Math.ceil(Math.random() * max);
 }
 
+// Sommo i numeri disponibili.
 let sommaNumeri = numeroUtente + getRandom(5);
-
 console.log("questa è la tua somma", sommaNumeri);
 
+// Dichiaro la variabile Booleana per la funzione Pari o Dispari.
 let odd = false;
 
+// Dichiaro la funzione pari o dispari.
 function isOdd (num){
     if(num % 2) odd = false;
     odd = true;
 }
 
+// Invoco la funzione pari o dispari sulla somma precedente.
 isOdd(sommaNumeri);
 
+// Stampo in pagina il vincitore.
+let output = document.querySelector("h1")
+
 if(sceltaUtente === "pari" && odd === true){
-    console.log("L'utente ha vinto!");
+    output.innerHTML = `La somma è: ${sommaNumeri}. L'utente ha scelto ${sceltaUtente} e quindi l'utente ha vinto!`;
 }else if(sceltaUtente === "dispari" && odd === false){
-    console.log("L'utente ha vinto!");
+    output.innerHTML = `La somma è: ${sommaNumeri}. L'utente ha scelto ${sceltaUtente} e quindi l'utente ha vinto!`;
 }else{
-    console.log("L'utente ha perso :(");
+    output.innerHTML = `La somma è: ${sommaNumeri}. L'utente ha scelto ${sceltaUtente} e quindi l'utente ha perso!`;
 }
