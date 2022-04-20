ee938cc1279a27aa5c2c8011cf1aff5a25d75285
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-20 16:58:20 +0200

    Fine base funzionamento Pari&Dispari

--- paridispari/script.js
+++ paridispari/script.js
@@ -7,7 +7,7 @@
     Bonus: L’inserimento avviene tramite un campo input
 */
 
-// const sceltaUtente = prompt("Scegli pari o dispari");
+const sceltaUtente = prompt("Scegli pari o dispari");
 const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
 function getRandom (max){
@@ -18,3 +18,19 @@ let sommaNumeri = numeroUtente + getRandom(5);
 
 console.log("questa è la tua somma", sommaNumeri);
 
+let odd = false;
+
+function isOdd (num){
+    if(num % 2) odd = false;
+    odd = true;
+}
+
+isOdd(sommaNumeri);
+
+if(sceltaUtente === "pari" && odd === true){
+    console.log("L'utente ha vinto!");
+}else if(sceltaUtente === "dispari" && odd === false){
+    console.log("L'utente ha vinto!");
+}else{
+    console.log("L'utente ha perso :(");
+}
