7621c1f6c81421db4c61ce190178ca8725da7bb5
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-20 18:03:20 +0200

    Inizio esercizio Palidroma

---
+++ palindromo/script.js
@@ -0,0 +1,22 @@
+/*
+    Palidroma
+    Chiedere all’utente di inserire una parola
+    Creare una funzione per capire se la parola inserita è palindroma
+    Bonus: L’inserimento avviene tramite un campo input
+*/
+
+const parolaUtente = prompt("Inserisci una parola.");
+
+isPalindroma(parolaUtente);
+
+// function isPalindroma (stringa){
+//     let arrayParola = []
+//     arrayParola.push(stringa.split(""));
+
+//     console.log(stringa);
+//     console.log(arrayParola);
+
+//     for(let i=0; i<stringa.length; i++){
+//         if(arrayParola[i] === )
+//     }
+// }
